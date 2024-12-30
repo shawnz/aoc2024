a2e2cc137360ec8a5bc632505f4df1fbf575031f
Shawn Zivontsis <shawnz@shawnz.org>
2024-12-30 13:24:50 -0500

    Try new approach with structs

--- src/bin/day6_part1.rs
+++ src/bin/day6_part1.rs
@@ -1,6 +1,5 @@
 use std::io::{self, Read};
 
-#[derive(Copy, Clone)]
 enum Direction {
     Up,
     Right,
@@ -13,115 +12,156 @@ enum MapTile {
     Obstructed,
 }
 
-fn rotate_90(dir: Direction) -> Direction {
-    match dir {
-        Direction::Up => Direction::Right,
-        Direction::Right => Direction::Down,
-        Direction::Down => Direction::Left,
-        Direction::Left => Direction::Up,
+impl Direction {
+    fn rotate_90(self: &Self) -> Self {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Right => Direction::Down,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+        }
     }
 }
 
-fn move_in_dir(
+struct Size {
     width: usize,
     height: usize,
+}
+
+struct Position {
     x: usize,
     y: usize,
-    dir: Direction,
-) -> Option<(usize, usize)> {
-    match dir {
-        Direction::Up => {
-            if y >= 1 {
-                Some((x, y - 1))
-            } else {
-                None
+}
+
+impl Position {
+    fn move_in_dir(self: &Self, size: &Size, dir: &Direction) -> Option<Self> {
+        match dir {
+            Direction::Up if self.y >= 1 => Some(Position {
+                x: self.x,
+                y: self.y - 1,
+            }),
+            Direction::Right if self.x < size.width - 1 => Some(Position {
+                x: self.x + 1,
+                y: self.y,
+            }),
+            Direction::Down if self.y < size.height - 1 => Some(Position {
+                x: self.x,
+                y: self.y + 1,
+            }),
+            Direction::Left if self.x >= 1 => Some(Position {
+                x: self.x - 1,
+                y: self.y,
+            }),
+            _ => None,
+        }
+    }
+}
+
+struct GuardData {
+    direction: Direction,
+    position: Position,
+}
+
+struct LabMap {
+    size: Size,
+    tiles: Vec<MapTile>,
+}
+
+struct LabData {
+    map: LabMap,
+    guard_data: Option<GuardData>,
+}
+
+impl LabData {
+    fn from_bytes(bytes: impl Iterator<Item = u8>) -> Self {
+        let mut first_line_width: Option<usize> = None;
+        let mut guard_data: Option<GuardData> = None;
+        let mut tiles: Vec<MapTile> = Vec::new();
+        for byte in bytes {
+            match byte {
+                b'.' => {
+                    tiles.push(MapTile::Empty { visited: false });
+                }
+                b'#' => {
+                    tiles.push(MapTile::Obstructed);
+                }
+                b'X' => {
+                    tiles.push(MapTile::Empty { visited: true });
+                }
+                b'^' | b'>' | b'v' | b'<' => {
+                    let i = tiles.len();
+                    guard_data = Some(GuardData {
+                        direction: match byte {
+                            b'^' => Direction::Up,
+                            b'>' => Direction::Right,
+                            b'v' => Direction::Down,
+                            b'<' => Direction::Left,
+                            _ => unreachable!(),
+                        },
+                        position: Position {
+                            x: first_line_width.map_or(i, |width| i % width),
+                            y: first_line_width.map_or(0, |width| i / width),
+                        },
+                    });
+                    tiles.push(MapTile::Empty { visited: true });
+                }
+                b'\n' => {
+                    if first_line_width.is_none() {
+                        first_line_width = Some(tiles.len());
+                    }
+                }
+                _ => {}
             }
         }
-        Direction::Right => {
-            if x < width - 1 {
-                Some((x + 1, y))
-            } else {
-                None
-            }
+        let width = first_line_width.unwrap_or_else(|| tiles.len());
+        let height = tiles.len() / width;
+        let size = Size { width, height };
+        LabData {
+            map: LabMap { size, tiles },
+            guard_data,
         }
-        Direction::Down => {
-            if y < height - 1 {
-                Some((x, y + 1))
-            } else {
-                None
-            }
-        }
-        Direction::Left => {
-            if x >= 1 {
-                Some((x - 1, y))
-            } else {
-                None
-            }
-        }
+    }
+}
+
+impl LabMap {
+    fn get_mut_tile_by_idx(self: &mut Self, index: usize) -> Option<&mut MapTile> {
+        self.tiles.get_mut(index)
+    }
+
+    fn get_mut_tile(self: &mut Self, pos: &Position) -> Option<&mut MapTile> {
+        self.get_mut_tile_by_idx(pos.x + pos.y * self.size.width)
     }
 }
 
 fn main() {
     let mut distinct_pos = 1u32;
-    let mut first_line_width: Option<usize> = None;
-    let mut orig_guard_dir: Option<Direction> = None;
-    let mut orig_guard_idx: Option<usize> = None;
-    let mut tiles: Vec<MapTile> = Vec::new();
-    for byte in io::stdin().bytes().filter_map(Result::ok) {
-        match byte {
-            b'.' => {
-                tiles.push(MapTile::Empty { visited: false });
-            }
-            b'#' => {
-                tiles.push(MapTile::Obstructed);
-            }
-            b'X' => {
-                tiles.push(MapTile::Empty { visited: true });
-            }
-            b'^' | b'>' | b'v' | b'<' => {
-                orig_guard_dir = match byte {
-                    b'^' => Some(Direction::Up),
-                    b'>' => Some(Direction::Right),
-                    b'v' => Some(Direction::Down),
-                    b'<' => Some(Direction::Left),
-                    _ => unreachable!(),
-                };
-                orig_guard_idx = Some(tiles.len());
-                tiles.push(MapTile::Empty { visited: true });
-            }
-            b'\n' => {
-                if first_line_width.is_none() {
-                    first_line_width = Some(tiles.len());
-                }
-            }
-            _ => {}
-        }
-    }
-    let width = first_line_width.unwrap_or_else(|| tiles.len());
-    let height = tiles.len() / width;
-    let mut guard_dir = orig_guard_dir.unwrap();
-    let mut guard_x = orig_guard_idx.unwrap() % width;
-    let mut guard_y = orig_guard_idx.unwrap() / width;
-    loop {
-        if let Some((new_x, new_y)) = move_in_dir(width, height, guard_x, guard_y, guard_dir) {
-            let new_idx = new_x + new_y * width;
-            match &mut tiles[new_idx] {
-                MapTile::Empty { visited } => {
-                    guard_x = new_x;
-                    guard_y = new_y;
-                    if !*visited {
-                        distinct_pos += 1;
-                        *visited = true;
+    let mut lab_data = LabData::from_bytes(io::stdin().bytes().filter_map(Result::ok));
+    if let Some(ref mut guard_data) = lab_data.guard_data {
+        loop {
+            if let Some(new_pos) = guard_data
+                .position
+                .move_in_dir(&lab_data.map.size, &guard_data.direction)
+            {
+                match lab_data.map.get_mut_tile(&new_pos) {
+                    Some(MapTile::Empty { ref mut visited }) => {
+                        guard_data.position = new_pos;
+                        if !*visited {
+                            distinct_pos += 1;
+                            *visited = true;
+                        }
                     }
+                    Some(MapTile::Obstructed) => {
+                        guard_data.direction = guard_data.direction.rotate_90();
+                    }
+                    None => unreachable!(),
                 }
-                MapTile::Obstructed => {
-                    guard_dir = rotate_90(guard_dir);
-                }
+            } else {
+                // left the mapped area
+                break;
             }
-        } else {
-            // left the mapped area
-            break;
         }
+    } else {
+        eprintln!("No guard found on map");
     }
     println!("{distinct_pos}")
 }
